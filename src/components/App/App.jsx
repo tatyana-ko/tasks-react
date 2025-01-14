a22d5d612f757f0e8e6be1988b19418467cb8fae
Tatyana Koba <misskotanyaz@gmail.com>
2025-01-14 14:41:45 +0300

    add counter solution

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,21 +4,20 @@ import Layout from "../Layout/Layout";
 import Counter from "../Counter/Counter";
 import Greeting from "../Greeting/Greeting";
 import News from "../News/News";
-import PageNotFound from "../PageNotFound/PageNotFound";
 import UserProfileForm from "../Form/UserProfileForm";
 
 const initialData = {
-  displayName: 'John Doe',
-  email: 'john@example.com',
-  phone: '+1234567890',
+  displayName: "John Doe",
+  email: "john@example.com",
+  phone: "+1234567890",
   emailNotifications: true,
   smsNotifications: false,
 };
 
 const handleSubmit = async (profile) => {
   // Имитация отправки данных на сервер
-  await new Promise(resolve => setTimeout(resolve, 1000));
-  console.log('Submitted profile:', profile);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  console.log("Submitted profile:", profile);
 };
 
 export default function App() {
@@ -29,8 +28,15 @@ export default function App() {
         <Route path="counter" element={<Counter exerciseName="Push-ups" />} />
         <Route path="greeting" element={<Greeting />} />
         <Route path="news" element={<News />} />
-        <Route path="user-form" element={<UserProfileForm initialData={initialData} onSubmit={handleSubmit}/>} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route
+          path="user-form"
+          element={
+            <UserProfileForm
+              initialData={initialData}
+              onSubmit={handleSubmit}
+            />
+          }
+        />
       </Route>
     </Routes>
   );
